db6efd9c70364f6903951081bae2f113203a195b
MONO ᵈᵉᵛ <68543161+Mono-94@users.noreply.github.com>
2023-05-15 05:12:13 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,39 @@
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+lua54 'yes'
+
+version '1.0.0'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/*.lua'
+}
+
+client_scripts {
+    'functions.lua',
+    'client/*.lua'
+}
+
+shared_scripts {
+    'Config.lua',
+    '@ox_lib/init.lua',
+}
+
+
+files {
+    'locales/*.json'
+}
+
+provides {
+    'esx_society'
+}
+
+escrow_ignore {
+    'Config.lua',
+    'locales/*.json',
+    'functions.lua',
+    'server/discord.lua',
+    'server/esx_society.lua'
+}
