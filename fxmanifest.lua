e0bb21bcf86ccdbf57c5b495320a287b0182f5f1
MONO <68543161+Mono-94@users.noreply.github.com>
2023-05-22 22:43:28 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,34 +6,3 @@ lua54 'yes'
 
 version '1.0.2'
 
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'server/*.lua'
-}
-
-client_scripts {
-    'functions.lua',
-    'client/*.lua'
-}
-
-shared_scripts {
-    'Config.lua',
-    '@ox_lib/init.lua',
-}
-
-
-files {
-    'locales/*.json'
-}
-
-provides {
-    'esx_society'
-}
-
-escrow_ignore {
-    'Config.lua',
-    'locales/*.json',
-    'functions.lua',
-    'server/discord.lua',
-    'server/esx_society.lua'
-}
